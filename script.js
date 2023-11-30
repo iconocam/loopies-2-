e76e42080e7e6155914552a6501a56c9af1d0c57
iconocam <150183122+iconocam@users.noreply.github.com>
2023-11-30 17:20:40 -0500

    well time to try again

---
+++ script.js
@@ -0,0 +1,136 @@
+//Part 3: Feeling Loopy
+// select more specific data 
+let data = ['ID', 'Name', 'Occupation', 'Age'];
+console.log(data)
+// let nums= [data[2]]  select stuff
+// console.log(nums)
+
+// const  array1= [  
+//     "42",  
+//     'Bruce', 
+//     'Knight', 
+//     '41' 
+// ];
+// console.log(array1)
+
+// const array2 = [
+//     "57",  
+//     'Bob', 
+//     'Fry Cook', 
+//     '19' 
+// ];
+// console.log(array2);
+
+// const array3 = [
+//     "63",  
+//     'Blaine', 
+//     'Quiz Master', 
+//     '58' 
+// ];
+// console.log(array3);
+
+// const array4 = [
+//     "98",  
+//     'Bill', 
+//     'Doctors Assistant', 
+//     '26' 
+// ];
+// console.log(array4);
+
+
+// data.join();
+// let completeArray= {ID}; {Name}; {Occupation}; {Age};
+// console.log(data)
+// console.log(completeArray);
+
+
+
+// let a= [array1];
+// let b= [array2];
+// let c= a.concat(b);
+// console.log(c);
+
+let newString = "ID , NAME , OCCUPATION, AGE;\n42, Bruce, Knight, 41 \n57, Bob, Fry Cook, 19 \n63, Blaine, Quiz Master,58 \n98, Bill, Doctor's Assistant, 26"
+
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+
+let rows = newString.split("\n");
+
+for (let i = 0; i < rows.length; i++){
+    let cells = rows[i].split(",");
+    console.log(newString);
+}
+
+// let whole = (newString);
+// console.log(whole);
+
+let newString2 = "Index, Mass (kg), Spring 1 (m), Spring 2 (m);\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+
+newString2[0]="Index";
+console.log(newString2)
+let cell5 = "";
+let cell6 = "";
+let cell7 = "";
+let cell8 = "";
+
+let rows2 = newString2.split("\n");
+
+for (let i = 0; i < rows2.length; i++) {
+
+    let cells = rows2[i].split(","); 
+    cell1 += cells[5] + " "; 
+    cell2 += cells[6] + " "; 
+    cell3 += cells[7] + " ";
+    cell4 += cells[8] + " ";
+    console.log(cells);
+}
+let united= newString2.concat(',\n'+ newString);
+console.log(united);
+
+const parentArray = [
+    newString ];
+    
+
+console.log(parentArray);
+
+
+const parentArray2 = [
+    newString2
+];
+
+
+console.log(parentArray2);
+
+const joinedArray = [...parentArray, ...parentArray2];
+
+console.log(joinedArray);
+
+
+// console.log(united);
+
+const object = {
+    keyA:"ID , NAME , OCCUPATION, AGE;\n42, Bruce, Knight, 41 \n57, Bob, Fry Cook, 19 \n63, Blaine, Quiz Master,58 \n98, Bill, Doctor's Assistant, 26",
+    keyB: "Index, Mass (kg), Spring 1 (m), Spring 2 (m);\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+
+    
+
+};
+console.log(object)
+console.log(object.keyB);
+console.log(object.keyA);
+
+//key 1 and 2 for referencing the data
+
+let keyA ="ID , NAME , OCCUPATION, AGE;\n42, Bruce, Knight, 41 \n57, Bob, Fry Cook, 19 \n63, Blaine, Quiz Master,58 \n98, Bill, Doctor's Assistant, 26".toLowerCase();
+console.log(keyA);
+
+let keyB= "Index, Mass (kg), Spring 1 (m), Spring 2 (m);\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232".toLowerCase();
+console.log(keyB);
+
+console.log('i did so much but it repo broke :|');
+
+
+
